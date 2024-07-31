cfaff84e5517034cd66d3ce606607d032509628a
Hafsa-00 <159915448+Hafsa-00@users.noreply.github.com>
2024-07-31 11:28:45 +0500

    makig project

---
+++ index.js
@@ -0,0 +1,54 @@
+import inquirer from 'inquirer';
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    student = [];
+    addStudent(obj) {
+        this.student.push(obj);
+    }
+}
+let persons = new Person();
+const programStart = async (persons) => {
+    do {
+        console.log('Welcome!');
+        let ans = await inquirer.prompt({
+            name: 'select',
+            type: 'list',
+            message: 'whom would you like to interact with?',
+            choices: ['staff', 'student', 'exit']
+        });
+        if (ans.select == 'staff') {
+            console.log('you approach the staff room');
+        }
+        else if (ans.select == 'student') {
+            const ans = await inquirer.prompt({
+                name: 'student',
+                type: 'input',
+                message: 'enter the student you want to talk',
+            });
+            const student = persons.student.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`hello i am ${name.name}.nice to meet you`);
+                console.log('new student added');
+                console.log(`current student lists:  `);
+                console.log(persons.student);
+            }
+            else {
+                console.log(`hello i am ${student.name}.nice to meet you again`);
+                console.log(`existing student lists:`);
+                console.log(persons.student);
+            }
+        }
+        else if (ans.select == 'exit') {
+            console.log('exiting the program.........');
+            process.exit();
+        }
+    } while (true);
+};
+programStart(persons);
